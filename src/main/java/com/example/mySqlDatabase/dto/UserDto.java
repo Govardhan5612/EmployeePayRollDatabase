e3dcb014c743a85f091b6cf6ab60778264bd70e9
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-05 12:50:40 +0530

    Adding startDate, notes and departments in database table

--- src/main/java/com/example/mySqlDatabase/dto/UserDto.java
+++ src/main/java/com/example/mySqlDatabase/dto/UserDto.java
@@ -1,14 +1,20 @@
 package com.example.mySqlDatabase.dto;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.JoinColumn;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class UserDto {
     @Pattern(regexp = "[A-Z]{1}[a-z]{2,}",message = "Invalid First name")
     @NotEmpty(message = "Not take empty values")
     private String firstName;
-    @NotEmpty(message = "Not take empty values")
+    @NotEmpty(message = "Not give empty values")
     @Pattern(regexp = "[A-Z]{1}[a-z]{2,}",message = "Invalid Last name")
     private String lastName;
     @NotBlank(message = "Not take blank values")
@@ -16,6 +22,10 @@ public class UserDto {
     private String gender;
     @NotNull(message = "Not take null input")
     @Min(value = 15000,message = "Minimum salary is 15000")
-    @Max(value = 50000,message = "Maximum salary is 50000")
+    @Max(value = 100000,message = "Maximum salary is 100000")
     private long salary;
+    private LocalDate startDate;
+    private String notes;
+    private List<String> departments;
+
 }
