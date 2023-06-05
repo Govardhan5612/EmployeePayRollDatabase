f820347b9cfd5244c77bb51489be035a4fe158f4
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-05 13:07:45 +0530

    Ability to validate the new fields in database table

--- src/main/java/com/example/mySqlDatabase/dto/UserDto.java
+++ src/main/java/com/example/mySqlDatabase/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.example.mySqlDatabase.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.JoinColumn;
@@ -11,21 +12,28 @@ import java.util.List;
 
 @Data
 public class UserDto {
-    @Pattern(regexp = "[A-Z]{1}[a-z]{2,}",message = "Invalid First name")
+    @Pattern(regexp = "[A-Z]{1}[a-z]{2,}", message = "Invalid First name")
     @NotEmpty(message = "Not take empty values")
     private String firstName;
     @NotEmpty(message = "Not give empty values")
-    @Pattern(regexp = "[A-Z]{1}[a-z]{2,}",message = "Invalid Last name")
+    @Pattern(regexp = "[A-Z]{1}[a-z]{2,}", message = "Invalid Last name")
     private String lastName;
     @NotBlank(message = "Not take blank values")
-    @Pattern(regexp = "male|female|Male|Female",message = "Invalid gender details only take male/female")
+    @Pattern(regexp = "male|female|Male|Female", message = "Invalid gender details only take male/female")
     private String gender;
     @NotNull(message = "Not take null input")
-    @Min(value = 15000,message = "Minimum salary is 15000")
-    @Max(value = 100000,message = "Maximum salary is 100000")
+    @Min(value = 15000, message = "Minimum salary is 15000")
+    @Max(value = 100000, message = "Maximum salary is 100000")
     private long salary;
+    @JsonFormat(pattern = "DD MM YEAR")
+    @NotNull(message = "This field is not null")
+    @PastOrPresent(message = "Enter past date")
     private LocalDate startDate;
+    @NotBlank(message = "note cannot be empty")
     private String notes;
+    @NotBlank(message = "note cannot be empty")
+    private String profilePic;
+    @NotNull(message = "departments should not null")
     private List<String> departments;
 
 }
