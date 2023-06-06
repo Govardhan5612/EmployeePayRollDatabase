90170e66502b1a6b9ef781b59d4cc6f0c21f0d84
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-06 16:30:41 +0530

    Ability to create another enviroment and run the application

--- src/main/java/com/example/mySqlDatabase/MySqlDatabaseApplication.java
+++ src/main/java/com/example/mySqlDatabase/MySqlDatabaseApplication.java
@@ -4,14 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.juli.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class MySqlDatabaseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySqlDatabaseApplication.class, args);
-		log.info("Employee payroll Application is started");
+		ApplicationContext context = SpringApplication.run(MySqlDatabaseApplication.class, args);
+		log.info("MySqlDatabaseApplication app started in {} Environment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
