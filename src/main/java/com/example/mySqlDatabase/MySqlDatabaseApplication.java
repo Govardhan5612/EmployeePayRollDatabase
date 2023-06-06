66c94744c30a318196dea790a10ff72fa4db7d48
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2023-06-06 15:39:10 +0530

    Ability to use Lombok library for logging

--- src/main/java/com/example/mySqlDatabase/MySqlDatabaseApplication.java
+++ src/main/java/com/example/mySqlDatabase/MySqlDatabaseApplication.java
@@ -1,13 +1,17 @@
 package com.example.mySqlDatabase;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MySqlDatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySqlDatabaseApplication.class, args);
+		log.info("Employee payroll Application is started");
 	}
 
 }
